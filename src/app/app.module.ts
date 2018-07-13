f3cd465f19c224d4cc3cfb68e157664d8faa147b
Abubakar Sambo <sadiqasg@gmail.com>
2018-07-13 16:27:05 +0100

    modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkersComponent } from './workers/workers.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { CustomersComponent } from './customers/customers.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { UsersComponent } from './users/users.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SignupComponent } from './signup/signup.component';
+import { PaymentplanComponent } from './paymentplan/paymentplan.component';
+import { LoginComponent } from './login/login.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    WorkersComponent,
+    SidenavComponent,
+    CustomersComponent,
+    VendorsComponent,
+    UsersComponent,
+    ReportsComponent,
+    SignupComponent,
+    PaymentplanComponent,
+    LoginComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
